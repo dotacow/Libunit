12d95851f34e4e724b3e8aef7c640e3b82d98ddb
dotacow <ykitaneh22@gmail.com>
2025-07-18 16:55:34 +0300

    tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -6,11 +6,16 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 16:09:50 by yokitane          #+#    #+#             */
-/*   Updated: 2025/07/18 16:12:23 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/07/18 16:42:45 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef TESTS_H
+# define TESTS_H
 
 int	ok_test (void);
 int	ko_test (void);
 int	signal_test (void);
 int test_launcher(void);
+
+#endif
